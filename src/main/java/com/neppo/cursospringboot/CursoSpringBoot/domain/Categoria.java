0d2fc617ae8332c961494a7069b9c14579e5f312
Kelwin Henrique <kelwin-hike@hotmail.com>
2018-04-15 20:31:20 -0300

    Banco de dados H2 e criação automática da tabela Categoria

--- src/main/java/com/neppo/cursospringboot/CursoSpringBoot/domain/Categoria.java
+++ src/main/java/com/neppo/cursospringboot/CursoSpringBoot/domain/Categoria.java
@@ -1,10 +1,17 @@
 package com.neppo.cursospringboot.CursoSpringBoot.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable{//Serializable para que a classe consiga ser tranformada em bytes e ser transmitida por rede
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 

f90c4f217a147211c0ee0700d9a5eac8cb32a7a3
Kelwin Henrique <kelwin-hike@hotmail.com>
2018-04-15 21:06:10 -0300

    Criando operação de instanciação

--- src/main/java/com/neppo/cursospringboot/CursoSpringBoot/CursoSpringBootApplication.java
+++ src/main/java/com/neppo/cursospringboot/CursoSpringBoot/CursoSpringBootApplication.java
@@ -1,12 +1,31 @@
 package com.neppo.cursospringboot.CursoSpringBoot;
 
+import com.neppo.cursospringboot.CursoSpringBoot.domain.Categoria;
+import com.neppo.cursospringboot.CursoSpringBoot.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoSpringBootApplication {
+public class CursoSpringBootApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }

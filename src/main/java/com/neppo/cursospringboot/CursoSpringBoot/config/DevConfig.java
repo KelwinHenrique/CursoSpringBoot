29fdde9cbdda35af9f55c8bc0728b7669d7a61bb
Kelwin Henrique <kelwin-hike@hotmail.com>
2018-07-17 10:06:29 -0300

    Configuração e envio do projeto para produção

---
+++ src/main/java/com/neppo/cursospringboot/CursoSpringBoot/config/DevConfig.java
@@ -0,0 +1,35 @@
+package com.neppo.cursospringboot.CursoSpringBoot.config;
+
+
+import com.neppo.cursospringboot.CursoSpringBoot.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+
+public class DevConfig {
+
+
+    @Autowired
+    private DBService dbService;
+
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws Exception {
+
+        if ("create".equals(strategy))
+            dbService.instantiateTestDatabase();
+
+        return true;
+    }
+
+
+
+}

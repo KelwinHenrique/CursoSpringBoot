f1cdd6ee8a12cd1082fda53929c1f91fa2f23e66
Kelwin Henrique <kelwin-hike@hotmail.com>
2018-07-17 09:06:30 -0300

    Criando profile de test

---
+++ src/main/java/com/neppo/cursospringboot/CursoSpringBoot/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.neppo.cursospringboot.CursoSpringBoot.config;
+
+
+import com.neppo.cursospringboot.CursoSpringBoot.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+
+public class TestConfig {
+
+
+    @Autowired
+    private DBService dbService;
+    @Bean
+    public boolean instantiateDatabase() throws Exception {
+
+        dbService.instantiateTestDatabase();
+
+        return true;
+    }
+
+
+
+}
